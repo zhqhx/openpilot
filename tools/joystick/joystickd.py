d4ad1548ffe92a689d46b7a2473b94bf612426ef
Shane Smiskol <shane@smiskol.com>
2024-08-30 22:29:55 -0700

    complicated and doesn't work (get_gamepad) is blocking
    old-commit-hash: bfeac7e8999cc738f8b46d4682a6e0c38491dfc7

--- tools/joystick/joystickd.py
+++ tools/joystick/joystickd.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
+import os
+import time
+import json
 import argparse
 import threading
 from dataclasses import dataclass
 from inputs import devices, get_gamepad
 
 import cereal.messaging as messaging
+from openpilot.common.basedir import BASEDIR
 from openpilot.common.realtime import Ratekeeper
 from openpilot.common.numpy_fast import interp, clip
 from openpilot.common.params import Params
@@ -14,15 +18,17 @@ from openpilot.tools.lib.kbhit import KBHit
 @dataclass
 class Gamepad:
   cancel_button: str  # west button
-  accel_axis: str  # left thumb stick, vertical
+  accel_axis: str  # right trigger or right thumb stick, vertical
+  neg_accel_axis: str | None  # left trigger, optional
   steer_axis: str  # right thumb stick, horizontal
 
 
 JS_EXPO = 0.4
 GAMEPADS = {
-  'Sony Interactive Entertainment DualSense Wireless Controller': Gamepad('BTN_WEST', 'ABS_Y', 'ABS_RX'),
-  'Generic X-Box pad': Gamepad('BTN_NORTH', 'ABS_Y', 'ABS_RX'),
+  'Sony Interactive Entertainment DualSense Wireless Controller': Gamepad('BTN_WEST', 'ABS_RZ', 'ABS_Z', 'ABS_RX'),
+  'Generic X-Box pad': Gamepad('BTN_NORTH', 'ABS_RZ', 'ABS_Z', 'ABS_RX'),
 }
+CONFIG_FILE = os.path.join(BASEDIR, 'tools', 'joystick', 'config.json')
 
 
 class Keyboard:
@@ -53,7 +59,7 @@ class Keyboard:
 
 class Joystick:
   def __init__(self):
-    # Check for present and supported gamepad, all use different axes
+    # Check for present and supported gamepad, some use different axes
     if len(devices.gamepads) == 0:
       raise Exception("No gamepad found")
     if (gamepad := devices.gamepads[0]).name not in GAMEPADS:
@@ -70,6 +76,12 @@ class Joystick:
   def update(self):
     joystick_event = get_gamepad()[0]
     event = (joystick_event.code, joystick_event.state)
+
+    # remap second accel axis to negative accel if present
+    if event[0] == self.config.neg_accel_axis:
+      print(event)
+      event = (self.config.accel_axis, -event[1])
+
     if event[0] == self.config.cancel_button:
       if event[1] == 1:
         self.cancel = True
@@ -106,6 +118,38 @@ def joystick_thread(joystick):
     joystick.update()
 
 
+def clear_queue():
+  time.sleep(1)
+  # t = time.monotonic()
+  # while (time.monotonic() - t) < 1:
+  #   get_gamepad()
+
+
+def calibrate_gamepad():
+  print('\n⚠️ Calibrating gamepad, press a button to bind to CANCEL...')
+  while not (event := get_gamepad()[0]).code.startswith('BTN_'):
+    pass
+  cancel_button = event.code
+  print(f'✅ Recorded CANCEL button: {cancel_button}')
+  clear_queue()
+
+  print('\nMove the right thumb stick to the right to bind to STEER...')
+  # note that we get a lot of spurious events with noisy trigger inputs, so we need to wait for a large diff:
+  while not ((event := get_gamepad()[0]).code.startswith('ABS_') and abs(event.state) > 200):
+    pass
+  steer_axis = event.code
+  print(f'✅ Recorded STEER axis: {steer_axis}')
+
+  clear_queue()
+
+  print('\nMove the left thumb stick up to bind to ACCEL...')
+  while not ((event := get_gamepad()[0]).code.startswith('ABS_') and abs(event.state) > 200):
+    pass
+  accel_axis = event.code
+  print(f'✅ Recorded ACCEL axis: {accel_axis}')
+  print('✅ Done!')
+
+
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Publishes events from your joystick to control your car.\n' +
                                                'openpilot must be offroad before starting joysticked.',
@@ -113,9 +157,9 @@ if __name__ == '__main__':
   parser.add_argument('--keyboard', action='store_true', help='Use your keyboard instead of a joystick')
   args = parser.parse_args()
 
-  if not Params().get_bool("IsOffroad") and "ZMQ" not in os.environ:
-    print("The car must be off before running joystickd.")
-    exit()
+  # if not Params().get_bool("IsOffroad") and "ZMQ" not in os.environ:
+  #   print("The car must be off before running joystickd.")
+  #   exit()
 
   print()
   if args.keyboard:
@@ -127,5 +171,8 @@ if __name__ == '__main__':
   else:
     print('Using joystick, make sure to run cereal/messaging/bridge on your device if running over the network!')
 
-  joystick = Keyboard() if args.keyboard else Joystick()
-  joystick_thread(joystick)
+    if not os.path.exists(CONFIG_FILE):
+      calibrate_gamepad()
+
+  # joystick = Keyboard() if args.keyboard else Joystick()
+  # joystick_thread(joystick)
