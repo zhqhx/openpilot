e7c396a009e4c18df09a5d6a8a9533efcfe3adfe
Shane Smiskol <shane@smiskol.com>
2024-09-24 15:06:20 -0700

    stash

--- cereal/__init__.py
+++ cereal/__init__.py
@@ -5,5 +5,4 @@ CEREAL_PATH = os.path.dirname(os.path.abspath(__file__))
 capnp.remove_import_hook()
 
 log = capnp.load(os.path.join(CEREAL_PATH, "log.capnp"))
-car = capnp.load(os.path.join(CEREAL_PATH, "car.capnp"))
 custom = capnp.load(os.path.join(CEREAL_PATH, "custom.capnp"))
